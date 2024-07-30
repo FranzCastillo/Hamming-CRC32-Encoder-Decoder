b5ce0747fcc37023432521b75cc788325c488a34
Andrés Montoya <montoyaw1@gmail.com>
2024-07-30 13:12:08 -0600

    fix on socket connection

--- src/sender/Main.js
+++ src/sender/Main.js
@@ -18,55 +18,56 @@ async function main () {
         
         const socket = new net.Socket();
 
+        console.log('Awaiting connection to the server on '+ip+':'+port);
         socket.connect(port, ip, () => {
-            console.log('Connecting to the server on '+ip+':'+port);
+            console.log('Connected to the server on '+ip+':'+port);
+            while(true){
+                let valid = true;
+                const message = rl.question('Enter the message or type "EXIT" to leave: ')
+                if(message === 'EXIT'){
+                    break;
+                }
+                const encodedMessage = binary_encode(message);
+                console.log('Binary encoded message:', encodedMessage);
+    
+                let codes = encodedMessage.split(' ');
+                codes = codes.filter(code => code.length > 0);
+    
+                let encoded = [];
+    
+                if (algorithm === "crc") {
+                    encoded = codes.map(code => {
+                        const checksum = crc32(code, generator).toString(16);
+                        return code+checksum;
+                    })
+                    console.log('Encoded codes:', encoded);
+    
+                } else if (algorithm === "hamming") {
+                    encoded = codes.map(code => {
+                        const trimmedCode = code.replace('\r', '');
+                        const bits = trimmedCode.split('').map(Number);
+                        const first_half = bits.slice(0, 4);
+                        const second_half = bits.slice(4, 8);
+                        const first_result = hamming(first_half, 7, 4).join('')
+                        const second_result = hamming(second_half, 7, 4).join('')
+                        return first_result + second_result;
+                    });
+                    console.log('Encoded codes:', encoded);
+                } else {
+                    console.log('Invalid algorithm');
+                    valid = false;
+                }
+    
+                if(valid){
+                    transmit(encoded, algorithm, socket, noiseRate);
+                }
+                
+            }
         });
         socket.on('error', (err) => {
             console.error('Error: ' + err);
         });
         
-        while(true){
-            let valid = true;
-            const message = rl.question('Enter the message or type "EXIT" to leave: ')
-            if(message === 'EXIT'){
-                break;
-            }
-            const encodedMessage = binary_encode(message);
-            console.log('Binary encoded message:', encodedMessage);
-
-            let codes = encodedMessage.split(' ');
-            codes = codes.filter(code => code.length > 0);
-
-            let encoded = [];
-
-            if (algorithm === "crc") {
-                encoded = codes.map(code => {
-                    const checksum = crc32(code, generator).toString(16);
-                    return code+checksum;
-                })
-                console.log('Encoded codes:', encoded);
-
-            } else if (algorithm === "hamming") {
-                encoded = codes.map(code => {
-                    const trimmedCode = code.replace('\r', '');
-                    const bits = trimmedCode.split('').map(Number);
-                    const first_half = bits.slice(0, 4);
-                    const second_half = bits.slice(4, 8);
-                    const first_result = hamming(first_half, 7, 4).join('')
-                    const second_result = hamming(second_half, 7, 4).join('')
-                    return first_result + second_result;
-                });
-                console.log('Encoded codes:', encoded);
-            } else {
-                console.log('Invalid algorithm');
-                valid = false;
-            }
-
-            if(valid){
-                transmit(encoded, algorithm, socket, noiseRate);
-            }
-            
-        }
     } catch (err) {
         console.error('Error:', err);
     }
